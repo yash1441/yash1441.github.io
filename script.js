e3757c8d18ed4c829656ad08b80d5206b9558dc8
Simon <yash1441@yahoo.com>
2023-04-16 14:36:52 +0530

    Added sound effects for right navbar

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function myFunction() {
 }
 
 const Indicator = document.getElementById("Indicator");
+const mainmenuAudio = document.getElementById("mainmenu");
+const submenuhoverAudio = document.getElementById("submenuhover");
+const submenusubhoverAudio = document.getElementById("submenusubhover");
+const submenusubselectAudio = document.getElementById("submenusubselect");
+const rightNavbar = document.getElementById("right-navbar");
+const rightSubmenuOption1 = document.getElementById("submenu-option1");
+const rightSubmenuOption2 = document.getElementById("submenu-option2");
+const rightSubmenuOption3 = document.getElementById("submenu-option3");
+const rightSubmenuOption4 = document.getElementById("submenu-option4");
+const muteButton = document.getElementById("mute-button");
 
 function toggleUser() {
 	Indicator.style.transform = "translateX(8px)";
@@ -17,6 +27,7 @@ function toggleUser() {
 	friends.style.display = "block";
 	groups.style.display = "none";
 	contacts.style.display = "none";
+	submenusubselectAudio.play();
 }
 function toggleUsers() {
 	Indicator.style.transform = "translateX(66px)";
@@ -26,6 +37,7 @@ function toggleUsers() {
 	groups.style.display = "block";
 	friends.style.display = "none";
 	contacts.style.display = "none";
+	submenusubselectAudio.play();
 }
 function toggleHistory() {
 	Indicator.style.transform = "translateX(128px)";
@@ -35,6 +47,7 @@ function toggleHistory() {
 	groups.style.display = "none";
 	friends.style.display = "none";
 	contacts.style.display = "none";
+	submenusubselectAudio.play();
 }
 function toggleEnv() {
 	Indicator.style.transform = "translateX(185px)";
@@ -44,6 +57,7 @@ function toggleEnv() {
 	groups.style.display = "none";
 	friends.style.display = "none";
 	contacts.style.display = "block";
+	submenusubselectAudio.play();
 }
 
 //market toggle
@@ -81,19 +95,33 @@ function playTab(evt, tabname) {
 	evt.currentTarget.className += " active";
 }
 
-const audio = document.getElementById("my-audio");
-const muteButton = document.getElementById("mute-button");
-
 muteButton.addEventListener("click", function () {
-	if (audio.muted) {
-		audio.muted = false;
-		audio.volume = 0.1;
-		audio.play();
+	if (mainmenuAudio.muted) {
+		mainmenuAudio.muted = false;
+		mainmenuAudio.volume = 0.1;
+		mainmenuAudio.play();
 		muteButton.classList.remove("fa-volume-mute");
 		muteButton.classList.add("fa-volume-up");
 	} else {
-		audio.muted = true;
+		mainmenuAudio.muted = true;
 		muteButton.classList.remove("fa-volume-up");
 		muteButton.classList.add("fa-volume-mute");
 	}
+});
+
+rightNavbar.addEventListener("mouseenter", () => {
+	submenuhoverAudio.play();
+});
+
+rightSubmenuOption1.addEventListener("mouseenter", () => {
+	submenusubhoverAudio.play();
+});
+rightSubmenuOption2.addEventListener("mouseenter", () => {
+	submenusubhoverAudio.play();
+});
+rightSubmenuOption3.addEventListener("mouseenter", () => {
+	submenusubhoverAudio.play();
+});
+rightSubmenuOption4.addEventListener("mouseenter", () => {
+	submenusubhoverAudio.play();
 });
