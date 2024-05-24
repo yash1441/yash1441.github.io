d7217b9b3b8b92c63ae751164fd3ad4e94ef2e06
Simon <yash1441@yahoo.com>
2024-05-24 13:34:41 +0530

    Added copy icon

--- chat-valorant-rank/script.js
+++ chat-valorant-rank/script.js
@@ -5,7 +5,10 @@ const showRRChangeCheckbox = document.getElementById('showRRChange');
 const platformSelect = document.getElementById('platform');
 const generateButton = document.getElementById('generateButton');
 const generatedUrl = document.getElementById('generatedUrl');
+const copyIcon = document.getElementById('copyIcon');
 // const exampleResponse = document.getElementById('exampleResponse');
+
+copyIcon.style.display = 'none';
 
 generateButton.addEventListener('click', function () {
     let url = "https://splendid-groovy-feverfew.glitch.me/valorant";
@@ -35,4 +38,12 @@ generateButton.addEventListener('click', function () {
         default:
             generatedUrl.textContent = url;
     }
+
+    copyIcon.style.display = 'inline-block';
+});
+
+copyIcon.addEventListener('click', function () {
+    const generatedText = document.getElementById('generatedUrl');
+    if (generatedText.length <= 0) return;
+    navigator.clipboard.writeText(generatedText.textContent)
 });
